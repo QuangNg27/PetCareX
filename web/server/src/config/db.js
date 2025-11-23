13d12f8525174883299c365f6c980a7457c021a5
QuangNg <qag2702@gmail.com>
2025-11-24 00:13:57 +0700

    Add web folder structure

---
+++ web/server/src/config/db.js
@@ -0,0 +1,14 @@
+const sql = require("mssql");
+
+const config = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    options: {
+        encrypt: true,
+        trustServerCertificate: true
+    }
+};
+
+module.exports = new sql.ConnectionPool(config).connect();
